5fffec8c6bf550175099fdda2f8e383b160bfdbd
Tariq Mohammad <tariq@Tariqs-MacBook-Pro.local>
2017-03-23 10:47:53 +0800

    Initial

---
+++ Test/Test/Service.swift
@@ -0,0 +1,24 @@
+//
+//  Service.swift
+//  Test1
+//
+//  Created by Tariq Mohammad on 3/23/17.
+//  Copyright © 2017 Tariq Mohammad. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+/**
+ A `ServerType` that requests data from an API webservice.
+ */
+public struct Service {
+    
+    /// Login authentication
+    public func login(_ userName: String,
+                      password: String,
+                      _ onCompletion: @escaping (_ error: Error?) -> Void) {
+        
+        
+    }
+}
